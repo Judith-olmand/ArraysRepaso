46c93f22869db3d78220f8bb4ee6e2ff98f04176
Judith-olmand <judith.olmand@educa.jcyl.es>
2026-03-04 09:21:38 +0100

    Añadidos en clase

--- src/Ejercicio13.java
+++ src/Ejercicio13.java
@@ -10,7 +10,8 @@ public class Ejercicio13 {
         System.out.println("Introduce un número: ");
         int n1 = sc.nextInt();
         int[][] matriz = new int[n1][n1];
-
+        //int[][] matriz = {{1,2},{2,0}};
+/*
         for (int i = 0; i < n1; i++) {
             for (int j = 0; j < n1; j++) {
                 matriz[i][j] = rand.nextInt(10);
@@ -18,19 +19,37 @@ public class Ejercicio13 {
             }
             System.out.println();
         }
+*/
+        int contador = 0;
+        int contador2 = 0;
+        while (contador < n1) {
+            while (contador2 < n1) {
+                matriz[contador][contador2] = rand.nextInt(10);
+                System.out.print(matriz[contador][contador2] + " ");
+                contador2 ++;
+            }
+            System.out.println();
+            contador++;
+            contador2 = 0;
+        }
 
-        System.out.println();
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                if (matriz[i][j] != matriz[j][i]){
+        /**
+         * los bucles continuan mientras i sea menor que la longitud
+         * de la matriz y miesntras simetrica siga siendo true
+         */
+        for (int i = 0; i < matriz.length && simetrica; i++) {
+            for (int j = i + 1; j < matriz[i].length; j++) { // solo mitad superior
+                if (matriz[i][j] != matriz[j][i]) {
                     simetrica = false;
                 }
             }
         }
+
+
         if (simetrica) {
-            System.out.println("La matriz es simétrica");
+            System.out.println("La matriz es simétrica.");
         } else {
-            System.out.println("La matriz no es simétrica");
+            System.out.println("La matriz NO es simétrica.");
         }
     }
 }
---
+++ src/FrecuenciaValoresArray.java
@@ -0,0 +1,53 @@
+/**
+ * Crea un programa java que trabaje con un array de 10 posiciones
+ * en el que cada elemento será un número entero comprendido entre
+ * 0 y 9 (ambos incluidos).
+ * Crea un array de frecuencias donde cada posición i indica
+ * cuántas veces aparece el número i en el array original
+ */
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class FrecuenciaValoresArray {
+    public static  void main(String args[]) {
+        Random aleatorio = new Random();
+        int contador = 0;
+        int[] primerArray = new int[10];
+        int [] frecuencias = new int[10];
+
+        System.out.println("Array");
+        for (int i = 0; i < primerArray.length; i++) {
+            primerArray[i] = aleatorio.nextInt(10);
+            System.out.print(primerArray[i] + " ");
+        }
+
+        for (int i = 0; i < primerArray.length; i++) {
+            for (int j = 0; j < frecuencias.length; j++) {
+                if (primerArray[j] == i){
+                    contador++;
+                }
+            }
+            frecuencias[i] = contador;
+            contador = 0;
+        }
+
+        int max = 0;
+
+        System.out.println();
+        System.out.println("Frecuencias");
+        for (int i = 0; i < frecuencias.length; i++) {
+            System.out.print(frecuencias[i] + " ");
+            if (frecuencias[i] > max){
+                max = frecuencias[i];
+            }
+        }
+
+        System.out.println();
+        for (int j = 0; j < frecuencias.length; j++) {
+            if (frecuencias[j] == max){
+                System.out.println("El número " + j + " es el que más veces aparece");
+            }
+        }
+    }
+}
